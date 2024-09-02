50fc0f700f855bc04030b373a6938572f7d3f101
Jason Zhang <zhangjt@umich.edu>
2024-09-02 10:04:51 -0400

    moving more processing into setup

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup(){
   angleMode(DEGREES);
   stroke(255);
   strokeWeight(12);
-  frameRate(20);
+  frameRate(15);
 
   // randomSeed(70);
   for(let i = 0; i < 160; i++){
@@ -43,15 +43,18 @@ function setup(){
     wave.push([]);
     for(let x = 0; x < width; x += res){
       for(let y = 0; y < height; y += res){
+
         let distances = [];
+
         for(let i = 0; i < points[f].length; i++){
           let d = ((x-points[f][i].x)**2)/randScales[i]+((y-points[f][i].y)**2);
           distances[i] = d;
         }
+
         let noise = floor(min(distances));
-        if (noise > maxNoise) {
-          maxNoise = noise;
-        }
+
+        maxNoise = max(noise, maxNoise);
+
         let index = x + y * width;
         wave[f][index] = noise;
       }
@@ -59,19 +62,27 @@ function setup(){
     console.log('Generating frame data: '+str(f+1)+'/'+str(points.length));
   }
   pixelDensity(1);
+
   console.log('maxNoise: ', maxNoise)
 
+  for(let f = 0; f < frmLen; f++){
+    for(let x = 0; x < width; x += res){
+      for(let y = 0; y < height; y += res){
+        let index = x + y * width;
+        const n = wave[f][index];
+        wave[f][index] = floor(map((n**1.32)/10, 0, maxNoise, len, 0));
+      }
+    }
+  }
 }
 
 function draw(){
   let frameIndex = frameCount % frmLen;
   background('#0827F5');
-
   for (let i = 0; i < width; i += res) {
     for (let j = 0; j < height; j += res) {
       let index = i + j * width;
-      const n = wave[frameIndex][index]
-      const charIndex = floor(map((n**1.32)/10, 0, maxNoise, len, 0));
+      let charIndex = wave[frameIndex][index];
       noStroke();
       fill(255);
       textSize(res);
@@ -86,9 +97,9 @@ function draw(){
   
 }
 
-function mousePressed() {
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-    let fs = fullscreen();
-    fullscreen(!fs);
-  }
-}
+// function mousePressed() {
+//   if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+//     let fs = fullscreen();
+//     fullscreen(!fs);
+//   }
+// }
